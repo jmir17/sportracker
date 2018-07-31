3f08b3369aa22e0ebc7131ca25fe6288ea4bb856
J Mir <jmir.17@gmail.com>
2018-07-31 19:47:45 +0200

    Create models

---
+++ src/models/measure.ts
@@ -0,0 +1,4 @@
+export default interface Measure{
+    date: Date,
+    measure: Number
+}
---
+++ src/models/training.ts
@@ -0,0 +1,7 @@
+import Measure from "./measure";
+
+export default interface Training{
+    id: Number,
+    name: String,
+    measures: Array<Measure>
+}
